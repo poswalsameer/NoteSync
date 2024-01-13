4ed3d08fedf6da519a1ea8175dd73ce9c54fda16
poswalsameer <poswalsameer2003@gmail.com>
2024-01-13 22:53:47 +0530

    Setup of react and tailwind

---
+++ notesync/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className=' m-10 flex justify-center items-center text-8xl text-yellow-300' >
+            NOTE SYNC
+      </div>
+
+      
+    </>
+  );
+}
+
+export default App;
